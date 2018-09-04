08e2084fe0d798f1c6bc03b7d2803f536e3c9f9f
Denis Boyda <boyda_d@mail.ru>
2018-09-04 17:50:37 +1000

    Matrix wirks

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,6 +1,94 @@
 #pragma once
 
-template <typename T, size_t defVal>
-class Matric{
+#include <unordered_map>
+#include <vector>
+
+
+ 
+template <typename T, T defaulVal>
+class Matrix{
+  using Point = std::pair<int, int>;
   
+  struct PointHash{
+    size_t operator()(const Point& p) const noexcept{
+      size_t hash = 17;
+      const size_t mult = 23;
+      hash = hash*mult + p.first;
+      hash = hash*mult + p.second;
+      return hash;
+    };
+  };
+ 
+  
+  class MatrixElement{
+    Matrix<T, defaulVal>& matrix;
+    
+  public:
+    MatrixElement(Matrix<T, defaulVal>& matrix): 
+          matrix(matrix){};
+    
+    void operator=(const T& other){
+      const Point key = std::make_pair(matrix.rowNum,
+            matrix.colNum);
+      if(other == defaulVal)
+        matrix.data.erase(key);
+      else
+        matrix.data[key] = other;
+      return;
+    }
+    
+    operator T() {
+      const Point key = std::make_pair(matrix.rowNum,
+            matrix.colNum);
+      const auto it = matrix.data.find(key);
+      if(it == matrix.data.end())
+        return defaulVal;
+      else
+        return it->second;      
+    }
+  };
+  
+  
+  class MatrixRow{ 
+    Matrix<T, defaulVal>& matrix;
+    
+  public:
+    MatrixRow(Matrix<T, defaulVal>& matrix): 
+          matrix(matrix) {};
+    
+    auto operator[](const int columNum){  
+      matrix.colNum = columNum;
+      return matrix.element;
+    }
+  };
+  
+  
+  std::unordered_map<Point, T, PointHash> 
+        data;
+  MatrixRow helper;
+  MatrixElement element;
+  int rowNum;
+  int colNum;
+  
+  public:
+    
+    Matrix(): helper(*this), element(*this) {};
+    
+    auto& operator[](const int i){      
+      rowNum = i;
+      return this->helper;
+    }
+    
+    auto begin(){
+      return std::begin(data);
+    }
+    
+    auto end(){
+      return std::end(data);  
+    }
+    
+    int size(){
+      return data.size();
+    }
+    
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,31 @@
 
 int main(){
 	
-	
-	
+  Matrix<int, 0> m;
+  
+  for(int i=0; i<10; i++)
+    m[i][i] = i;
+  
+  for(int i=0; i<10; i++)
+    m[0+i][9-i] = 9-i;
+  
+  for(int i=1; i<9; i++){
+    for(int j=1; j<9; j++){
+      int tmp = m[i][j];
+      std::cout << tmp << ' ';
+    }
+    std::cout << '\n';
+  }
+  
+  std::cout << "size: " << m.size() << '\n';
+
+  for(const auto& e: m){
+    const int x = e.first.first;
+    const int y = e.first.second;
+    const int v = e.second;
+    std::cout << x << ' ' << y << ' ' << v << '\n';
+  }
+  
+  
   return 0;
 }
