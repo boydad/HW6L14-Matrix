4cb3334a41f4e71266eca470338113fc79d147a2
Denis Boyda <boyda_d@mail.ru>
2018-09-11 22:30:47 +1000

    Revert "Revert "Revert "Empty"""
    This reverts commit 0d83c2b859c1951c64ec93a684503b21777ea46f.

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <tuple>
+#include "Matrix.h"
+
+int main(){
+	
+  Matrix<int, 0> m;
+  
+  Matrix<int, 0> m1 = m;
+  
+  
+  for(int i=0; i<10; i++)
+    m[i][i] = i;
+  
+  for(int i=0; i<10; i++)
+    m[0+i][9-i] = 9-i;
+  
+  for(int i=1; i<9; i++){
+    for(int j=1; j<9; j++){
+      std::cout << m[i][j] << ' ';
+    }
+    std::cout << '\n';
+  }
+  
+  std::cout << "size: " << m.size() << '\n';
+
+  for(const auto& e: m){
+    const int x = e.first.first;
+    const int y = e.first.second;
+    const int v = e.second;
+    std::cout << x << ' ' << y << ' ' << v << '\n';
+  }
+
+  return 0;
+}
